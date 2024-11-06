062b9e5b4e1a63a201ae80855951ec0ed02e3a4a
malena kiryluk <malenakiryluk@gmail.com>
2024-11-06 15:51:03 -0300

    correcciones

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,11 @@
+process.loadEnvFile("./src/.env")
+
 const config = {
-    PORT:8080,
-    MONGO_URL:"mongodb+srv://malenakiryluk:malena2014!@cluster0.tab2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    DB_NAME:"proyectoCoder",
-    SECRET_SESSION: "MalenaCoder123",
-    SECRET: "malenaCoder"
+    PORT:process.env.PORT,
+    MONGO_URL:process.env.MONGO_URL,
+    DB_NAME:process.env.DB_NAME,
+    SECRET_SESSION: process.env.SECRET_SESSION,
+    SECRET: process.env.SECRET
 }
 
 module.exports={config}
